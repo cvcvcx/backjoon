cc17818d67968fc5330106cb49aa5e11bb6c7c00
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-01 19:13:52 +0900

    [Bronze II] Title: OX퀴즈, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,15 @@
+t = int(input())
+for i in range(t):
+    ox_list = list(input())
+    score = 0
+    sum_score = 0
+
+    for ox in ox_list:
+        if ox == "O":
+            score +=1
+            sum_score += score
+        else:
+            score = 0
+    print(sum_score)
+
+
