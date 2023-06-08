ce1df23c7a97d9c51d4a8b0ac0224c7c082caf7f
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-08 14:17:41 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,16 @@
+def sum(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return int(a/b)
+def sur(a,b):
+    return a%b
+a, b = map(int, input().split())
+print(sum(a, b))
+print(sub(a, b))
+print(mul(a, b))
+print(div(a, b))
+print(sur(a, b))
