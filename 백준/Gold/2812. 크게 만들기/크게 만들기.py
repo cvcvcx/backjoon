37654220ecdc58aa6c29811820c316c70c0d899b
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-09 18:01:28 +0900

    [Gold III] Title: 크게 만들기, Time: 180 ms, Memory: 38972 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -1,12 +1,11 @@
-N, K = map(int,input().split())
+n, k = map(int,input().split())
 number_list = list(input())
 stack = []
-cnt = K
+cnt = k
+
 for num in number_list:
     while stack and num>stack[-1] and cnt>0:
-        del stack[-1]
+        stack.pop()
         cnt -= 1
     stack.append(num)
-print(''.join(stack[:N-K]))
-    
-    
+print(''.join(stack[:n-k]))
