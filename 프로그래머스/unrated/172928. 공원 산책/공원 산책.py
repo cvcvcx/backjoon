0829d5e2a69d2ea8cdf897c71b1f4343f313dc31
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-16 02:07:56 +0900

    [unrated] Title: 공원 산책, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -1,79 +1,45 @@
-def make_park_list(park):
-    park_list = []
-    for item in park:
-        park_list.append(list(item))
-    return park_list
-
-def find_start(park_list,S):
-    for i in range(len(park_list)):
-        for j in range(len(park_list[i])):
-            if park_list[i][j] == S:
-                return [i,j]
-def set_dir_num(dir):
-    dir_num = 0
-    if dir == "N":
-        dir_num = 0
-    elif dir == "E":
-        dir_num = 1
-    elif dir == "S":
-        dir_num = 2
-    elif dir == "W":
-        dir_num = 3
-    return dir_num
-
-#now에서 route의 명령대로 이동 -> now에 이동한 값 대입
-def move(park_list,now,route):
-    # N = 0 E = 1 S = 2 W = 3 
-    directionY = [-1,0,1,0] 
-    directionX = [0,1,0,-1]
-    
-    dir, dis =  route.split()
-    dir_num = set_dir_num(dir)
-    start = now
-    calculation_end_Y = now[0]+(directionY[dir_num])*int(dis)
-    calculation_end_X = now[1]+(directionX[dir_num])*int(dis)
-    print("now = ",now)
-    print("calculation = ",[calculation_end_Y,calculation_end_X])
-    
-    if calculation_end_Y > len(park_list)-1 or calculation_end_Y < 0:
-        return start
-    
-    if calculation_end_X > len(park_list[0])-1 or calculation_end_X < 0:
-        return start
-    
-    for i in range(int(dis)):
-        calculationY = now[0]+(directionY[dir_num])*i
-        calculationX = now[1]+(directionX[dir_num])*i
-        
-        if park_list[calculationY][calculationX] == "X":
-            return start
-        
-    for _ in range(int(dis)):
-        calculationY = now[0]+(directionY[dir_num])
-        calculationX = now[1]+(directionX[dir_num])
-        if calculationY < len(park_list) and calculationY >-1 :
-            if calculationX < len(park_list[calculationY]) and calculationX >-1:
-                if park_list[calculationY][calculationX] == "O":
-                    now = [calculationY,calculationX]
-                elif park_list[calculationY][calculationX] == "S":
-                    now = [calculationY,calculationX]
-                elif park_list[calculationY][calculationX] == "X":
-                    now = start
-                
-    return now
-    
-    
 def solution(park, routes):
+    dy = {"N":-1,"E":0,"S":1,"W":0}
+    dx = {"N":0,"E":1,"S":0,"W":-1}
     
-    S = "S"
-    X = "X"
+    # 맵의 크기를 변수로 만든다.
+    N,M = len(park),len(park[0])
+    # 시작점을 찾는다
+    y,x=0,0
+    #이중배열을 순환하면서 S를 찾는다.
+    for i in range(N):
+        for j in range(M):
+            if park[i][j] == "S":
+                #찾으면 y,x에 인덱스를 집어넣는다.
+                y,x = i,j
     
-    park_list = make_park_list(park)
-    start = find_start(park_list,S)
-    now = start
-    
+    #routes를 순환하면서 이동거리와 방향을 가지고 이동시켜본다.
     for route in routes:
-        now = move(park_list,now,route)
+        dir_,dist = route.split()
+        #이동이 유효한지 판별하는 boolean값
+        move_is_false = False
         
-    return now
-
+        # 1~dist, 그러니까 목적지까지 직선으로 장애물이 있는지 없는지 확인 및 맵을 벗어나는지 확인
+        for i in range(1, int(dist)+1):
+            ny,nx = y+dy[dir_]*i,x+dx[dir_]*i
+            
+            #맵 밖으로 나갔는지 확인
+            if ny < 0 or nx < 0 or ny > N-1 or nx > M-1:
+                move_is_false = True
+                break
+            #X를 지나가는지 확인
+            if park[ny][nx] == "X":
+                move_is_false = True
+                break
+            
+        if move_is_false == True:
+            continue
+        else:
+            ny,nx = y+dy[dir_]*int(dist),x+dx[dir_]*int(dist)
+            y,x = ny,nx
+    
+            
+    
+    
+    answer = [y,x]
+    return answer
