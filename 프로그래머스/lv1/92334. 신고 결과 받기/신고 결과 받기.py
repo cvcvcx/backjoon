56313d4ef9b0f556f7d9fb4f67ac2600f70de8d8
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-16 01:30:13 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,13 +1,16 @@
 def solution(id_list, report, k):
     answer = [0] * len(id_list)
-    reported_id_dic = {id:0 for id in id_list}
+    reported_id_dic = {id:[] for id in id_list}
     
     #신고당한 유저의 dic의 value에 += 1
     for r in set(report):
-        reported_id_dic[r.split()[1]] += 1
-    #set(report)에 있는 값들을 순회하면서 신고횟수가 k 이상인 값을 찾기
-    for r in set(report):
-        if reported_id_dic[r.split()[1]] >= k:
-            answer[id_list.index(r.split()[0])] += 1
+        report_man = r.split()[0]
+        reported_man = r.split()[1]
+        reported_id_dic[reported_man].append(report_man)
+    #dic.items()에 있는 값들을 순회하면서 신고횟수가 k 이상인 값을 찾기
+    for key,value in reported_id_dic.items():
+        if len(value) >= k:
+            for j in value:
+                answer[id_list.index(j)] += 1
     
     return answer
