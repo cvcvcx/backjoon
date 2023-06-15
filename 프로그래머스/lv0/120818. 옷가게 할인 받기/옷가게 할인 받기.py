1ee30089110e85df08b5dfa57fcbd5d455a73d67
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-15 17:21:24 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,10 +1,10 @@
 def solution(price):
-    sale_percent = 0
-    if(price>=500000):
-        sale_percent = 20
-    elif(price>=300000):
-        sale_percent = 10
-    elif(price>=100000):
-        sale_percent = 5
-
-    return int(price*((100-sale_percent)/100))
+    sale = 1
+    if price >= 500000:
+        sale = 0.8
+    elif price >= 300000:
+        sale = 0.9
+    elif price >= 100000:
+        sale = 0.95
+    
+    return int(price*sale)
