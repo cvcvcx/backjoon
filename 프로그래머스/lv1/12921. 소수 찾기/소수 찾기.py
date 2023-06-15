08fa694afcc658a9a345cf4a0c94ba7947dd8d73
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-15 17:49:25 +0900

    [level 1] Title: 소수 찾기, Time: 1014.19 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,6 +1,6 @@
 def solution(n):
-    answer = set(range(2,n+1))
-    
+    number_set = set(range(2,n+1))
     for i in range(2,n+1):
-        answer -= set(range(i*2,n+1,i))
-    return len(answer)
+        number_set -= set(range(i*2,n+1,i))
+    return len(number_set)
+    
