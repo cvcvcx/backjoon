cb57f1983399452a45d990264e29ec1385d5d9c7
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-15 18:16:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,13 +1,17 @@
 def solution(id_list, report, k):
     answer = [0]*len(id_list)
-    report_id_set_arr = [set() for _ in range(len(id_list))]
-    for i in range(len(report)):
-        reported_man = report[i].split()[1]
-        report_man = report[i].split()[0]
-        report_id_set_arr[id_list.index(reported_man)].add(report_man)
-    for report_id_set in report_id_set_arr:
-        if(len(report_id_set)>=k):
-            for report_id in report_id_set:
-                answer[id_list.index(report_id)] += 1
     
+    reported_man_set_arr = [set() for _ in range(len(id_list))]
+    
+    for r in report:
+        report_man = r.split()[0]
+        reported_man = r.split()[1]    
+        reported_man_set = reported_man_set_arr[id_list.index(reported_man)]
+        reported_man_set.add(report_man)
+        
+    for report_man_set in reported_man_set_arr:
+        if len(report_man_set) >= k:
+            for man in report_man_set:
+                answer[id_list.index(man)] += 1
+                
     return answer
