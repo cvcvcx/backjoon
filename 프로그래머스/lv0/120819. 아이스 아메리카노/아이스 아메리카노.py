0c9d42609ef5af1ffdd0d71f9d37c9c3d9a53771
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-15 17:23:46 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,7 +1,3 @@
 def solution(money):
-    answer = []
-    coffee_price = 5500
-    coffee_count = money // coffee_price
-    coin = money % coffee_price
-    answer=[coffee_count,coin]
-    return answer
+    price = 5500
+    return [money//5500,money%5500]
