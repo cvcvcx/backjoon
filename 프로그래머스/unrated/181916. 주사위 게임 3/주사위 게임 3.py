e6e32c848ca60d2cf13a5e93ba56ee262ac0656b
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-16 14:09:00 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,21 +1,23 @@
 def solution(a, b, c, d):
-    answer = 0
-    number_list = [a,b,c,d]
+    num_list = [a,b,c,d]
     count_list = []
-    
-    for i in number_list:
-        count_list.append(number_list.count(i))
-    
+    answer = 0
+    for n in num_list:
+        count_list.append(num_list.count(n))
     if max(count_list) == 4:
-        answer = a * 1111
+        answer = a*1111
     elif max(count_list) == 3:
-        answer = (10 *number_list[count_list.index(3)] + number_list[count_list.index(1)]) ** 2
+        p = num_list[count_list.index(3)]
+        q = num_list[count_list.index(1)]
+        answer = (10 * p + q) ** 2
     elif max(count_list) == 2:
         if min(count_list) == 2:
-            answer = (a + c) * abs(a - c) if a == b else (a + b) * abs(a - b)
+            answer = (a+c) * abs(a-c) if a == b else (a+b) * abs(a-b)
         else:
-            p = number_list[count_list.index(2)]
-            answer = (a*b*c*d)/p**2
+            p = num_list[count_list.index(2)]
+            
+            answer = a*b*c*d / p ** 2
     else:
-        answer = min(number_list)
+        answer = min(num_list)
+            
     return answer
