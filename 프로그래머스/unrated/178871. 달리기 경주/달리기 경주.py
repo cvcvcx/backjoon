561bed8a532bcb80ce5a2978e2d899571a34c0d2
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-16 12:24:39 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,16 +1,16 @@
 def solution(players, callings):
-    player_dic = {player:i for i, player in enumerate(players)}
+    p_rank_dict = {player: i for i,player in enumerate(players)}
+    rank_p_dict = {i: player for i,player in enumerate(players)}
     
     for call in callings:
-        cur_idx = player_dic[call]
-        pre_idx = cur_idx-1
+        cur_idx = p_rank_dict[call]
+        pre_idx = cur_idx - 1
         
-        player_dic[call] -= 1
-        player_dic[players[pre_idx]] += 1
+        cur_player = call
+        pre_player = rank_p_dict[pre_idx]
         
-        temp = players[pre_idx]
-        players[pre_idx] = players[cur_idx]
-        players[cur_idx] = temp
-        
-        
-    return players
+        rank_p_dict[pre_idx],rank_p_dict[cur_idx] = rank_p_dict[cur_idx],rank_p_dict[pre_idx]
+        p_rank_dict[cur_player],p_rank_dict[pre_player] = p_rank_dict[pre_player], p_rank_dict[cur_player]
+    
+    answer = list(rank_p_dict.values())
+    return answer
