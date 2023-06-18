bec82bda0b97647126af5e84e566abce7ec7b4b7
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-18 09:14:41 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+def solution(number):
+    answer = 0
+    for i in range(len(number)):
+        for j in range(i,len(number)):
+            for k in range(j, len(number)):
+                if number[i]+number[j]+number[k] == 0 and i != j and j != k:
+                    answer += 1
+    return answer
