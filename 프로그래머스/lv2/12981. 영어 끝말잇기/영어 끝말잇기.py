ae109694975c448f6b9d5c6bfb3dc491907a91a0
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-19 17:32:45 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,6 @@
+def solution(n, words):
+    answer = [0,0]
+    for i in range(1,len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
+            return [(i%n)+1,(i//n)+1]
+    return answer
