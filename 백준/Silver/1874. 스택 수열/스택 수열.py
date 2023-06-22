cab472ccaa94597e1fa5367c0a9a4e550ad8e835
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-22 20:26:13 +0900

    [Silver II] Title: 스택 수열, Time: 4032 ms, Memory: 32840 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,27 @@
+n = int(input())
+# 숫자가 들어가는 스택
+stack = []
+# +,- 기호가 들어가는 리스트
+answer = []
+# 스택으로 만들 수 있는 수열인지 판단하는 플래그
+flag = 0
+# 지금 스택에 들어갈 숫자
+cur = 1
+for _ in range(n):
+    num = int(input())
+    while cur <= num:
+        stack.append(cur)
+        answer.append("+")
+        cur += 1
+        
+    if stack[-1] == num:
+        stack.pop()
+        answer.append("-")
+    else:
+        print("NO")
+        flag = 1
+        break
+if flag == 0:        
+    for i in range(len(answer)):
+        print(answer[i])
+            
