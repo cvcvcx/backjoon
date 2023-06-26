5c9ea6af016963d53c283eda8633db7962596b34
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-26 16:44:42 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 47340 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,29 +1,12 @@
-import sys
-input = sys.stdin.readline
 N = int(input())
-A = list(map(int,input().split()))
-A.sort()
+A = set(map(int,input().split()))
 
 M = int(input())
-B = list(map(int,input().split()))
 
-for b in B:
-    #인덱스를 탐색하는 경우에는 0부터 시작하게끔 할 것
-    #자연수를 탐색하는 경우에는 1부터 시작하게
-    start = 0
-    end = len(A) - 1
-    flag = 0
-    
-    while start <= end:
-        mid = (start + end) // 2
-        if b == A[mid]:
-            flag = 1
-            print(1)
-            break
-        elif b > A[mid]:
-            start = mid + 1
-        else:
-            end = mid - 1            
-    if flag != 1:
+B = map(int,input().split())    
+
+for n in B:
+    if n in A:
+        print(1)
+    else:
         print(0)
-    
