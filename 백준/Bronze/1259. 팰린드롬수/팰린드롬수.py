f3b0f644084834706a45102177205930c01d6e30
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-06-26 15:20:22 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,17 +1,8 @@
-import sys
-input = sys.stdin.readline
 while True:
-    n = str(int(input().strip()))
-    if n=="0":
+    num  = input()
+    if num == "0":
         break
-        
-    count_ = 0
-    for i in range(len(n)//2):
-        if n[i] == n[-(i+1)]:
-            continue
-        else:
-            count_ += 1
-    if count_ == 0:
+    if num == num[::-1]:
         print("yes")
     else:
         print("no")
