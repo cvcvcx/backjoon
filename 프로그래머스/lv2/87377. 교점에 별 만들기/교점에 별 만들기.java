951f491e0ad247e2d86c7afde1e54bc3872a0147
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-15 12:00:37 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.28 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,46 +1,43 @@
+
 import java.util.*;
 class Solution {
-    //좌표를 나타낼 클래스 설정
-    //생성자에 주의한다.
     private static class Point{
-        public final long x,y;
+        public final long x;
+        public final long y;
         
         public Point(long x, long y){
             this.x = x;
             this.y = y;
         }
     }
-    //교점의 좌표를 구하는 함수
-    // a1 + b1 + c1 = 0,
-    // a2 + b2 + c2 = 0 으로 교점을 구하는 공식을 바꿔서 생각한다.
-    // (b1 * c2 - c1 * b2)/(a1 * b2 - b1 * a2)
-    // (c1 * a2 - a1 * b2)/(a1 * b2 - b1 * a2)
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2){
-        double x = (double) (b1 * c2 - c1 * b2)/(a1 * b2 - b1 * a2);
-        double y = (double) (c1 * a2 - a1 * c2)/(a1 * b2 - b1 * a2);
-        
+    // 1. 두 직선의 교점을 구한다. - 공식에 맞춰서 변수를 지정해서 헷갈림이 덜하도록 한다.
+    private Point intersection(long A, long B, long E, long C, long D, long F){
+        double x = (double) (B*F - E*D)/(A*D - B*C);
+        double y = (double) (E*C - A*F)/(A*D - B*C);
+        // 정수만 값을 가지게 한다.
         if (x % 1 != 0 || y % 1 != 0){
             return null;
         }
         
-        return new Point((long) x,(long) y);
+        return new Point((long) x, (long) y);
     }
-    
+    // 3. 최고점 최저점을 구한다. 아직까진 변환되지 않았으니까, long자료형을 써야한다.
     private Point getMinPoint(List<Point> points){
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
         
-        for (Point p: points){
+        for (Point p : points){
             x = p.x < x ? p.x : x;
             y = p.y < y ? p.y : y;
         }
         return new Point(x,y);
     }
+    
     private Point getMaxPoint(List<Point> points){
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
         
-        for (Point p: points){
+        for (Point p : points){
             x = p.x > x ? p.x : x;
             y = p.y > y ? p.y : y;
         }
@@ -49,43 +46,44 @@ class Solution {
     
     public String[] solution(int[][] line) {
         List<Point> points = new ArrayList<>();
-        
-        for (int i = 0; i < line.length; i++){
-            for (int j = i + 1; j <line.length; j++){
+        // 2.정수를 저장한다. 두 개의 직선을 모두 점검해야하므로, 이 패턴이 등장한다.
+        // 경우의 수를 따질 때 등장하는 패턴(조합? 순열?)
+        for (int i = 0; i<line.length; i++){
+            for (int j = i+1; j<line.length; j++){
                 Point intersection = intersection(line[i][0],line[i][1],line[i][2],line[j][0],line[j][1],line[j][2]);
                 if (intersection != null){
                     points.add(intersection);
                 }
             }
         }
+        // 3.최고점과 최저점을 구한다. 실제로 있는 좌표를 구하는 것이 아니라, x의 최고점, 최저점, y의 최고점 최저점을 구해서 좌표로 받는것이다.
         
         Point minimum = getMinPoint(points);
         Point maximum = getMaxPoint(points);
-        
-        int width = (int) (maximum.x - minimum.x + 1);
-        int height = (int) (maximum.y - minimum.y + 1);
-        
-        System.out.println(width);
-        System.out.println(height);
+        // 4. 최고점 최저점을 이용해서 맵을 만든다. 이때부터 int로 변환된다.
+        // 배열의 크기를 반환하는 것이므로, +1을 해주는 것을 잊지말자!
+        int width = (int)(maximum.x - minimum.x+1);
+        int height = (int)(maximum.y - minimum.y+1);
         
         char[][] arr = new char[height][width];
-        
-        
-        
-        for(char[] row: arr){
+        // .으로 모든 좌표를 채워넣는다.
+        for (char[] row : arr){
             Arrays.fill(row,'.');
         }
         
+        // 5. 교점과 최고점, 최저점을 이용해서 맵에 맞는 좌표에 *을 찍는다.
         for (Point p : points){
-            int x = (int) (p.x - minimum.x);
-            int y = (int) (maximum.y - p.y);
+            int x = (int)(p.x - minimum.x);
+            int y = (int)(maximum.y - p.y);
+            
             arr[y][x] = '*';
         }
+        // 6. String 배열타입으로 char배열을 변환한다.
+        String[] answer = new String[arr.length];
         
-        String[] result = new String[arr.length];
-        for (int i = 0; i < result.length; i++){
-            result[i] = new String(arr[i]);
+        for (int i = 0; i<arr.length; i++){
+            answer[i] = new String(arr[i]);
         }
-        return result;
+        return answer;
     }
 }
