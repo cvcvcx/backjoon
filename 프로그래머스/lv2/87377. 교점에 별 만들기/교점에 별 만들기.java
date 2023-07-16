bb9aaec5af155cceac37ea2ec4f65e35076a0e43
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-17 08:49:02 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.19 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,28 +1,40 @@
-
 import java.util.*;
 class Solution {
+    /*
+    1. 아이디어
+        - 교점을 구한다.
+        - 교점에서 정수로 나타낼 수 있는 수만 리스트에 추가한다.
+        - 리스트에서 가장 큰 수와 가장 작은 수를 이용해 맵 배열의 크기를 구한다. +1하는거 조심
+        - 맵 배열을 채운다.
+        - 가장 큰 수와 가장 작은 수, 교점을 이용해서 새로운 배열에 교점을 찍는다.
+        - String배열로 만들어 반환한다.
+    
+    2. 자료구조
+    
+        Point 클래스 생성 - long타입 x, y를 가진 Point클래스를 생성
+        PointList 생성 - List<Point>를 통해 새로운 리스트를 생성
+        맵 배열 생성 - char[][]를 통해 문자열을 구성할 맵 생성
+        String answer - char[][]를 통해 String[]을 생성
+    */
     private static class Point{
-        public final long x;
-        public final long y;
+        public final long x, y;
         
         public Point(long x, long y){
             this.x = x;
             this.y = y;
         }
     }
-    // 1. 두 직선의 교점을 구한다. - 공식에 맞춰서 변수를 지정해서 헷갈림이 덜하도록 한다.
     private Point intersection(long A, long B, long E, long C, long D, long F){
         double x = (double) (B*F - E*D)/(A*D - B*C);
         double y = (double) (E*C - A*F)/(A*D - B*C);
-        // 정수만 값을 가지게 한다.
+        
         if (x % 1 != 0 || y % 1 != 0){
             return null;
         }
-        
-        return new Point((long) x, (long) y);
+        return new Point((long) x,(long) y);
     }
-    // 3. 최고점 최저점을 구한다. 아직까진 변환되지 않았으니까, long자료형을 써야한다.
-    private Point getMinPoint(List<Point> points){
+    
+    private Point getMinValue(List<Point> points){
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
         
@@ -31,9 +43,9 @@ class Solution {
             y = p.y < y ? p.y : y;
         }
         return new Point(x,y);
+        
     }
-    
-    private Point getMaxPoint(List<Point> points){
+    private Point getMaxValue(List<Point> points){
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
         
@@ -42,12 +54,13 @@ class Solution {
             y = p.y > y ? p.y : y;
         }
         return new Point(x,y);
+        
     }
+    
+    
     
     public String[] solution(int[][] line) {
         List<Point> points = new ArrayList<>();
-        // 2.정수를 저장한다. 두 개의 직선을 모두 점검해야하므로, 이 패턴이 등장한다.
-        // 경우의 수를 따질 때 등장하는 패턴(조합? 순열?)
         for (int i = 0; i<line.length; i++){
             for (int j = i+1; j<line.length; j++){
                 Point intersection = intersection(line[i][0],line[i][1],line[i][2],line[j][0],line[j][1],line[j][2]);
@@ -56,34 +69,28 @@ class Solution {
                 }
             }
         }
-        // 3.최고점과 최저점을 구한다. 실제로 있는 좌표를 구하는 것이 아니라, x의 최고점, 최저점, y의 최고점 최저점을 구해서 좌표로 받는것이다.
+        Point minimum = getMinValue(points);
+        Point maximum = getMaxValue(points);
         
-        Point minimum = getMinPoint(points);
-        Point maximum = getMaxPoint(points);
-        // 4. 최고점 최저점을 이용해서 맵을 만든다. 이때부터 int로 변환된다.
-        // 배열의 크기를 반환하는 것이므로, +1을 해주는 것을 잊지말자!
-        int width = (int)(maximum.x - minimum.x+1);
-        int height = (int)(maximum.y - minimum.y+1);
+        int width = (int)(maximum.x - minimum.x + 1);
+        int height = (int)(maximum.y - minimum.y + 1);
         
         char[][] arr = new char[height][width];
-        // .으로 모든 좌표를 채워넣는다.
         for (char[] row : arr){
             Arrays.fill(row,'.');
         }
         
-        // 5. 교점과 최고점, 최저점을 이용해서 맵에 맞는 좌표에 *을 찍는다.
         for (Point p : points){
             int x = (int)(p.x - minimum.x);
             int y = (int)(maximum.y - p.y);
-            
             arr[y][x] = '*';
         }
-        // 6. String 배열타입으로 char배열을 변환한다.
-        String[] answer = new String[arr.length];
         
+        String[] answer = new String[arr.length];
         for (int i = 0; i<arr.length; i++){
             answer[i] = new String(arr[i]);
         }
+        
         return answer;
     }
 }
