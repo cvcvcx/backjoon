cf8d72ef0f1aea9032d3d1ddb459f12876a1df7b
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-20 07:58:15 +0900

    [level 2] Title: 삼각 달팽이, Time: 14.69 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,6 +1,6 @@
 class Solution {
-    private static final int[] dx = {0,1,-1};
-    private static final int[] dy = {1,0,-1};
+    private final int[] dx = {0,1,-1};
+    private final int[] dy = {1,0,-1};
     
     public int[] solution(int n) {
         int[][] triangle = new int[n][n];
@@ -8,30 +8,27 @@ class Solution {
         int y = 0;
         int v = 1;
         int d = 0;
-        
         while (true){
             triangle[y][x] = v;
             v++;
-            
             int nx = x + dx[d];
             int ny = y + dy[d];
-            
-            if(ny==n || ny == -1||nx==n || nx == -1||triangle[ny][nx] != 0){
+            if(ny == n||ny<0 || nx == n ||nx<0 || triangle[ny][nx] != 0){
                 d = (d+1)%3;
                 nx = x + dx[d];
                 ny = y + dy[d];
-                if(ny==n || ny == -1||nx==n || nx == -1||triangle[ny][nx] != 0){
+                if(ny == n||ny<0 || nx == n ||nx<0 || triangle[ny][nx] != 0){
                     break;
                 }
             }
             x = nx;
             y = ny;
         }
-        
-        int[] answer = new int[v-1];
         int index = 0;
-        for (int i =0; i<n; i++){
-            for (int j =0; j<=i; j++){
+        int[] answer = new int[v-1];
+        
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<=i; j++){
                 answer[index] = triangle[i][j];
                 index++;
             }
