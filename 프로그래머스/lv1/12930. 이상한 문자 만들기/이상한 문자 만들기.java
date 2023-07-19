3b82aef7a50d924baffd415c3b5ffefc5a5e1708
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-19 14:45:27 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.10 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,19 +1,21 @@
 class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        boolean toUpper = true;
+        boolean isUpper = true;
         for (char c : s.toCharArray()){
             if(!Character.isAlphabetic(c)){
+                isUpper = true;
                 sb.append(c);
-                toUpper = true;
             }else{
-                if(toUpper){
+                if(isUpper){
                     sb.append(Character.toUpperCase(c));
+                    isUpper = false;
                 }else{
                     sb.append(Character.toLowerCase(c));
-                }
-                toUpper = !toUpper;
+                    isUpper = true;
+                }    
             }
+            
         }
         return sb.toString();
     }
