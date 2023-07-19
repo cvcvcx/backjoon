a40c95b4310d89db86a4f250ef9301167c23af35
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-19 17:14:41 +0900

    [level 1] Title: 시저 암호, Time: 2.27 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,22 +1,18 @@
 class Solution {
-    
     private char push(char c, int n){
+        if(!Character.isAlphabetic(c)){
+            return c;
+        }
         int offset = Character.isUpperCase(c) ? 'A':'a';
-        int num = (c + n - offset)%('Z'-'A'+1);
-        return (char)(num+offset);
+        int position = c - offset;
+        position = (position + n) % ('Z' - 'A' +1);
+        return (char) (position+offset);
     }
-    
     public String solution(String s, int n) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if(Character.isAlphabetic(c)){
-                sb.append(push(c,n));
-            }else{
-                sb.append(c);
-            }
+        for(char c : s.toCharArray()){
+            sb.append(push(c,n));
         }
-        
         return sb.toString();
     }
 }
