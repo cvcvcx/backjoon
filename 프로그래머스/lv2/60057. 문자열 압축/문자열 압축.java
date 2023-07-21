89f0a578ca01188bffa7f27ee55ae1450bf49b88
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-21 11:15:52 +0900

    [level 2] Title: 문자열 압축, Time: 0.15 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -1,24 +1,23 @@
 import java.util.*;
 class Solution {
     
-    private List<String> split(String source, int length){
+    private List<String> split(String s, int length){
         List<String> tokens = new ArrayList<>();
-        for(int i = 0; i<source.length(); i+=length){
-            int endIndex = i + length;
-            if(endIndex > source.length()){
-                endIndex = source.length();
-            }
-            tokens.add(source.substring(i,endIndex));
+        for(int i =0; i<s.length(); i+=length){
+            int endIndex = i+length;
+            if(endIndex>s.length()) endIndex = s.length();
+            tokens.add(s.substring(i,endIndex));
         }
         return tokens;
     }
     
-    private int compress(String source, int length){
-        String last = "";
+    private int compress(String s, int length){
+        
         StringBuilder sb = new StringBuilder();
+        String last = "";
         int count = 0;
-        List<String> tokens = split(source,length);
-        for(String str : tokens){
+        
+        for(String str : split(s,length)){
             if(str.equals(last)){
                 count++;
             }else{
@@ -34,17 +33,22 @@ class Solution {
             sb.append(count);
         }
         sb.append(last);
+        
         return sb.length();
     }
     
     public int solution(String s) {
-        int min = Integer.MAX_VALUE;
+        //단위별로 자른 문자배열을 만든다.
+        //단위별로 자른 문자배열을 가지고, 압축된 단어를 만든다.
+        //압축된 단어의 길이를 지금까지의 최소값과 비교한다.
+        //압축된 단어가 더 짧으면, 최소값으로 친다.
+        //1~s.length()까지를 돌면서, 단위를 새로 설정하면서 반복한다.
+        //substring에서는 (startIndex에서,endIndex의 -1인덱스까지 잘라서 반환하게되므로, s.length()까지 돌아야함)
         
-        for(int length = 1; length<=s.length(); length++){
+        int min = Integer.MAX_VALUE;
+        for (int length = 1; length<=s.length();length++){
             int compressed = compress(s,length);
-            if (compressed<min) {
-                min = compressed;
-            }
+            min = min>compressed ? compressed : min;
         }
         return min;
     }
