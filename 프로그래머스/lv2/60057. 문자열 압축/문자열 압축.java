4269baa6d43595b71ecbe7bd5648a2374a35debd
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-23 18:35:28 +0900

    [level 2] Title: 문자열 압축, Time: 0.08 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -2,54 +2,54 @@ import java.util.*;
 class Solution {
     
     private List<String> split(String s, int length){
-        List<String> tokens = new ArrayList<>();
-        for(int i =0; i<s.length(); i+=length){
-            int endIndex = i+length;
-            if(endIndex>s.length()) endIndex = s.length();
-            tokens.add(s.substring(i,endIndex));
+        List<String> result = new ArrayList<>();
+        for(int startIdx = 0; startIdx<s.length(); startIdx += length){
+            int endIdx = startIdx + length;
+            if(endIdx > s.length()){
+                endIdx = s.length();
+            }
+            result.add(s.substring(startIdx,endIdx));
         }
-        return tokens;
+        return result;
     }
     
-    private int compress(String s, int length){
-        
+    private int compress(List<String> words){
         StringBuilder sb = new StringBuilder();
         String last = "";
         int count = 0;
-        
-        for(String str : split(s,length)){
-            if(str.equals(last)){
+        for(String word: words){
+            if(last.equals(word)){
                 count++;
             }else{
                 if(count>1){
-                    sb.append(count);
+                    sb.append(count);    
                 }
                 sb.append(last);
-                last = str;
+                last = word;
                 count = 1;
             }
         }
         if(count>1){
-            sb.append(count);
+            sb.append(count);    
         }
         sb.append(last);
-        
         return sb.length();
     }
     
     public int solution(String s) {
-        //단위별로 자른 문자배열을 만든다.
-        //단위별로 자른 문자배열을 가지고, 압축된 단어를 만든다.
-        //압축된 단어의 길이를 지금까지의 최소값과 비교한다.
-        //압축된 단어가 더 짧으면, 최소값으로 친다.
-        //1~s.length()까지를 돌면서, 단위를 새로 설정하면서 반복한다.
-        //substring에서는 (startIndex에서,endIndex의 -1인덱스까지 잘라서 반환하게되므로, s.length()까지 돌아야함)
         
+        //s를 정해진 숫자대로 나누는 함수
         int min = Integer.MAX_VALUE;
-        for (int length = 1; length<=s.length();length++){
-            int compressed = compress(s,length);
-            min = min>compressed ? compressed : min;
+        
+        for(int i = 1; i<=s.length();i++){
+            List<String> words = split(s,i);
+            int compressed = compress(words);
+            min = compressed < min ? compressed : min;
         }
+        //나눈 리스트를 가지고 압축된 문자열을 만들어 개수를 반환하는 함수
+        
+        
+        
         return min;
     }
 }
