eab7a8fb6b816ff1e008e9ba4354af6f7315293c
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-23 11:22:18 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.04 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,78 +1,71 @@
 class Solution {
     private static final int[] dx = {0,1,-1,0};
     private static final int[] dy = {1,0,0,-1};
-    
-    //좌표의 사방에 사람이 있는지 확인한다.
-    private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude){
+    private boolean isNextPeople(char[][] room, int x, int y, int exclude){
+        // x,y좌표의 사방중 exclude를 제외하고, 나머지 방향에 사람이 있는지 확인
+        
         for(int d = 0; d<4; d++){
-            if(d == exclude) continue;
+            if (d == exclude) continue;
             
             int nx = x+dx[d];
             int ny = y+dy[d];
             
-            if(ny<0||ny>=room.length||nx<0||nx>=room[ny].length){
+            if(ny >= room.length|| ny<0 || nx>=room[ny].length || nx<0){
                 continue;
             }
-            
-            if(room[ny][nx] == 'P'){
-                return true;
-            }
+            if(room[ny][nx] == 'P') return true;
         }
         return false;
     }
     
-    //방의 x,y좌표에 있는 사람이 거리두기를 지키고 있는지 판단한다.
-    private boolean isDistanced(char[][] room, int x, int y){
-        // x, y좌표에서 사방에 사람이 있는지 확인을 해야한다.
-        // 사람이 있으면 바로 false를 반환하고, 사람이 없고 빈자리인 경우 그 사방에 사람이 존재하는지 확인해야함
+    private boolean isDistance(char[][] room, int x ,int y){
+        //방의 x, y좌표에있는 사람이 거리두기를 지키고 있는지 확인
         for(int d = 0; d<4; d++){
             int nx = x+dx[d];
             int ny = y+dy[d];
-            if(ny<0||ny>=room.length||nx<0||nx>=room[ny].length){
+            if(ny >= room.length||ny<0 || nx>=room[ny].length || nx<0){
                 continue;
             }
             switch(room[ny][nx]){
                 case 'P': return false;
-                case 'O': 
-                    if(isNextToVolunteer(room,nx,ny,3-d)){
-                        return false;
-                    }
+                case 'O':
+                    if(isNextPeople(room,nx,ny,3-d)) return false;
                     break;
             }
-            
         }
         return true;
+        
     }
     
-    //room을 받아서, 그 방이 거리두기를 지키고 있는지 판단
-    private boolean isDistanced(char[][] room){
-        //방의 거리두기를 판단한다.
-        //판단하기 위해서 각 좌표에 사람이 있는 경우에, 거리두기를 지키고 있는지 판단해야한다.
-        for(int y = 0; y<room.length; y++){
-            for(int x = 0; x<room[y].length; x++){
-                if(room[y][x] != 'P') continue;
-                if(!isDistanced(room,x,y)) return false;
+    
+    private boolean isDistance(char[][] room){
+        for(int i =0; i<room.length; i++){
+            for (int j =0; j<room[i].length; j++){
+                if(room[i][j] != 'P') continue;
+                if(!isDistance(room,j,i)){
+                    return false;
+                }
             }
         }
-        //모두 검사했는데 거리두기를 지키지 않은 사람이 나오지 않으면 true를 반환하게 된다.;
         return true;
     }
     
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
-        for (int i =0; i<answer.length; i++){
+        //거리두기 판별을 위해 맵을 생성
+        for(int i = 0; i<answer.length; i++){
             String[] place = places[i];
             char[][] room = new char[place.length][];
             for(int j = 0; j<room.length; j++){
                 room[j] = place[j].toCharArray();
             }
-            if(isDistanced(room)){
+            if(isDistance(room)) {
                 answer[i] = 1;
             }else{
                 answer[i] = 0;
             }
         }
-        
+        //생성한 맵을 가지고 거리두기에 적합한지 확인
         return answer;
     }
 }
