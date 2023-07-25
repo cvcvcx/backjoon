46e1f521a80b449929c68b92305d8d93705a3d5e
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-25 13:19:37 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 7.84 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,6 +1,5 @@
 class Solution {
-    //0의 개수와 1의 개수를 가진 객체 클래스 (압축된 범위의 1과 0의 개수를 세기 위한 클래스)
-    private static class Count{
+    public class Count{
         public final int zero;
         public final int one;
         
@@ -8,36 +7,37 @@ class Solution {
             this.zero = zero;
             this.one = one;
         }
+        
         public Count add(Count other){
-            return new Count(this.zero+other.zero,this.one+other.one);
-        }        
+            return new Count(this.zero+other.zero, this.one+other.one);
+        }
     }
-    //재귀함수 count arr의 범위에 있고, offsetX, offsetY,size를 압축하는 점화식과 종료조건을 표현해야함
-    private Count count(int[][] arr,int offsetX,int offsetY, int size){
-        int h = size/2;
-        for(int y = offsetY; y<offsetY+size; y++){
-            for(int x = offsetX; x<offsetX+size; x++){
-                if(arr[y][x]!=arr[offsetY][offsetX]){
-                    //사등분한 범위의 Count를 모두 합친 값을 반환
-                    return count(arr,offsetX,offsetY,h)
-                        .add(count(arr,offsetX+h,offsetY,h))
-                        .add(count(arr,offsetX,offsetY+h,h))
-                        .add(count(arr,offsetX+h,offsetY+h,h));
+    
+    private Count count(int x, int y, int[][] arr, int size){
+        
+        for(int i = y; i<y+size; i++){
+            for (int j = x; j<x+size; j++){
+                if(arr[i][j] != arr[y][x]){
+                    return count(x,y,arr,size/2)
+                        .add(count(x+size/2,y,arr,size/2))
+                        .add(count(x,y+size/2,arr,size/2))
+                        .add(count(x+size/2,y+size/2,arr,size/2));
                 }
             }
         }
-        if(arr[offsetY][offsetX] == 0){
-            return new Count(1,0);
-        }else{
+        if(arr[y][x] == 1){
             return new Count(0,1);
+        }else{
+            return new Count(1,0);
         }
+        
     }
     
-    
-    
     public int[] solution(int[][] arr) {
-        Count count = count(arr,0,0,arr.length);
-        return new int[] {count.zero,count.one};
         
+        Count count = count(0,0,arr,arr.length);
+        
+        int[] answer = {count.zero,count.one};
+        return answer;
     }
 }
