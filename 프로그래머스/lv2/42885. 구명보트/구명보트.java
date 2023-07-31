69da487b8caca03b6782ee33d6886b53c2fd1c01
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2023-07-31 10:31:52 +0900

    [level 2] Title: 구명보트, Time: 9.32 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;
+        int idx = 0;
+        for(int i = people.length-1; i >= idx; i--){
+            if(people[i]+people[idx]<=limit){
+                answer++;
+                idx++;
+            }else{
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
